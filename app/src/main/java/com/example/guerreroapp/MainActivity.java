430aa2a5d5ed59e21f53fd69399446602f87ec56
daniel <mr.bain@hotmail.com>
2019-05-04 00:37:05 -0500

    primeras lineas de codigo

--- app/src/main/java/com/example/guerreroapp/MainActivity.java
+++ app/src/main/java/com/example/guerreroapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.guerreroapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etusu;
+    private EditText etcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etusu = (EditText) findViewById(R.id.texnom);
+        etcon = (EditText) findViewById(R.id.texcon);
+
     }
 }

3cb57802df6c16f9939ca4b4636df51671857d68
daniel <mr.bain@hotmail.com>
2019-05-05 20:12:46 -0500

    agregado de vista registro agregado de funcionalidades de login y creado de cuentas

--- app/src/main/java/com/example/guerreroapp/MainActivity.java
+++ app/src/main/java/com/example/guerreroapp/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.guerreroapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*varables que se usaran dentro del activity
+    *@param etusu
+    * sera usado para saber el edittext del usuario
+    *@param etcon
+    * sera usado para saber el edittext de la contraseño
+    */
     private EditText etusu;
     private EditText etcon;
 
+    //metodo de creacion del activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +29,39 @@ public class MainActivity extends AppCompatActivity {
         etusu = (EditText) findViewById(R.id.texnom);
         etcon = (EditText) findViewById(R.id.texcon);
 
-    }
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
 
+        if( preferences.getInt("autenticado", 0) == 1){
+            Intent i = new Intent(this , menu.class);
+            startActivity(i);
+        }
+    }
+    //metopdo del boton login
     public void login(View view){
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        Toast toast;
+        String usuario =  preferences.getString("usuario","");
+        String contrasena  = preferences.getString("contraseña","");
+        if (!usuario.equals("") && !contrasena.equals("")){
+            if ( !usuario.equals(etusu.getText())){
+                toast = Toast.makeText(this, R.string.nousuario, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            if ( !contrasena.equals(etcon.getText())){
+                toast = Toast.makeText(this, R.string.nocontrasena, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }else{
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("autenticado", 1);
+            Intent i = new Intent(this, menu.class);
+            startActivity(i);
+        }
 
     }
 
     public void singin(View view){
-
+        Intent i = new Intent(this, Registro.class);
+        startActivity(i);
     }
 }
